b4abf81601fa49e48e6647dbe75a8ff204feba44
1MasonMuch <masonm126@nycstudents.net>
2025-09-25 06:23:39 -0700

    1

---
+++ Guessing.py
@@ -0,0 +1,16 @@
+# count = 1 
+
+# while count <= 5:
+#     print("This is loop number", count)
+#     count = count + 1
+
+# order = ""
+# while order != "done":
+#     order = input("What would you like ro order? (type 'done' to finish): ")
+# print ("Thanks for your order")
+
+#Challenge
+number = 10
+while number > 0: 
+     print("This is loop number", number)
+     number = number - 1
