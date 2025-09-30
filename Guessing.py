e82066d73bac831940689472173a96b783ab146d
1MasonMuch <masonm126@nycstudents.net>
2025-09-30 05:56:26 -0700

    Final Version (Completed)

--- Guessing.py
+++ Guessing.py
@@ -28,13 +28,17 @@ import random
 number = random.randint(1,10)
 answer = 0
 guess_history = []
+guesses = 0
 while answer != number:
      
      answer = int(input("Please guess a number 1-10"))
      guess_history.append(answer)
      if answer > number:
+          guesses = guesses + 1
           print(f"Lower than {answer}")
-          print(f"Your previous answers were {guess_history}")
+          print(f"Your previous answers were {guess_history} and you guessed {guesses} time(s)")
      elif answer < number:
+          guesses = guesses + 1
           print(f"Higher than {answer}")
-          print(f"Your previous answers were {guess_history}")
+          print(f"Your previous answers were {guess_history} and you guessed {guesses} time(s)")
+print(f"Congrats you got it right! It took you {guesses} trie(s)")
